951f1a629f72228adf2f68104f569dea5f65fa0d
Javier Vargas <javier1615@gmail.com>
2015-10-19 19:48:21 -0500

    game1 markup & animations

--- js/animation.js
+++ js/animation.js
@@ -8,3 +8,89 @@ $(function() {
 	TweenLite.to(bottom, 1.5, {width: "100%"});
 	TweenLite.to(left, 1.5, {height: "100%"});
 });
+
+
+// Game 1
+
+$(document).ready(function() {
+    $('.tooltip').tooltipster();
+    $('#startBtn').click(function(e){
+        e.preventDefault();
+    	$('#stage01').css({
+    	// 	"transform": "scale(.5)",
+  			// "transition": "transform 500ms"
+    	})
+    	$('#stage01').animate({
+    		left: "-=100%",
+    		opacity: "0"
+    	},800)
+    	$('#stage02').animate({
+    		left: "-=100%",
+    		opacity: "1"
+    	},800)
+    })
+    $('#nextBtn').click(function(e){
+        e.preventDefault();
+    	$('#stage02').css({
+    	// 	"transform": "scale(.5)",
+  			// "transition": "transform 500ms"
+    		// left: "-=100%",
+    		// opacity: "0"
+    	})
+    	$('#stage02').animate({
+    		left: "-=100%",
+    		opacity: "0"
+    	},800)
+    	$('#stage03').animate({
+    		left: "-=100%",
+    		opacity: "1"
+    	},800, function(){
+    		$('#stage03 .a-jugar').animate({
+    			marginTop: "450px"
+    		})
+    		$('#stage03 .stars-footer').animate({
+    			opacity: "1"
+    		})
+    	})
+    	setTimeout(function(e){
+	    	$('#stage03').animate({
+	    		left: "-=100%",
+	    		opacity: "0"
+	    	},800)
+	    	$('#stage04').animate({
+	    		left: "-=100%",
+	    		opacity: "1"
+	    	},800)
+    	},800+2000)
+    })
+    $('#answerBtn').click(function(e){
+        e.preventDefault();
+    	$('#stage04').css({
+    	// 	"transform": "scale(.5)",
+  			// "transition": "transform 500ms"
+    	})
+    	$('#stage04').animate({
+    		left: "-=100%",
+    		opacity: "0"
+    	},800)
+    	$('#stage05').animate({
+    		left: "-=100%",
+    		opacity: "1"
+    	},800)
+    })
+});
+
+$(document).ready(function($){
+    $("#nextBtn").hover(
+	  function () { $(this).addClass('animated bounce infinite');}, 
+	  function () { $(this).removeClass('animated bounce infinite');}
+	  );
+    $("#homeBtn").hover(
+	  function () { $(this).addClass('animated bounce infinite');}, 
+	  function () { $(this).removeClass('animated bounce infinite');}
+	  );
+    $("#retryBtn").hover(
+	  function () { $(this).addClass('animated bounce infinite');}, 
+	  function () { $(this).removeClass('animated bounce infinite');}
+	  );
+});
